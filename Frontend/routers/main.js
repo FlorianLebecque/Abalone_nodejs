7f9f11354161cf71582798d2d45aef5fc216b14a
Florian Lebecque <=>
2022-04-19 13:46:46 +0200

    add notice management

--- Frontend/routers/main.js
+++ Frontend/routers/main.js
@@ -8,15 +8,34 @@ MainRouter.get("/",async (req,res)=>{
 
     let options = ctrl.GetOption(req);
 
+    console.log(options);
+
     res.render("index.ejs",info = options);
 
 });
 
 MainRouter.get("/login",(req,res)=>{
 
-    let options = ctrl.GetOption(req);
+    let log_info = 0;
+    switch(req.query.i){
+        case "o":
+            log_info = 1;
+            break;
+        case "n":
+            log_info = 2;
+            break;
+        default:
+            log_info = 0;
+            break;
+    }
 
+    let options = ctrl.GetOption(req);
+    
     options["page"] = "login";
+    options["log_info"] = log_info;
+
+    console.log(options);
+
 
     res.render("index.ejs",info = options);
 

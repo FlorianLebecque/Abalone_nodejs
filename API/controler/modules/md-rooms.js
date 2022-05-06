f74b90e130138203105ef2b8c7f61e20e8098a89
Florian <Florianlebecque@hotmail.com>
2022-05-06 10:15:31 +0200

    added joining room action

--- API/controler/modules/md-rooms.js
+++ API/controler/modules/md-rooms.js
@@ -33,20 +33,19 @@ const md_rooms = {
 
     async CreateRoom(room_form){
 
-        
-
         let res = await this.checkToken(room_form.player,room_form.token)
         if(res){
 
             if(this.IsPlayerInRoom(room_form.player)){
-                throw {code:403,err:"Player is already in room"}; 
+                throw {code:403,err:"Player is already in a room"}; 
             }
 
             let new_room = {
                 id :    Str.uuid(),
                 player_1: room_form.player,
                 player_2: -1,
-                passwd  : -1
+                passwd  : -1,
+                started : false
             }
 
             this.rooms.set(new_room.id,new_room);
@@ -56,6 +55,25 @@ const md_rooms = {
 
         }else{
             throw {code:403,err:"Invalid user or token"}; 
+        }
+    },
+
+    async JoinRoom(room_form){
+        let res = await this.checkToken(room_form.player,room_form.token)
+        if(res){
+
+            if(this.IsPlayerInRoom(room_form.player)){
+                throw {code:403,err:"Player is already in a room"}; 
+            }
+
+            let room = this.rooms.get(room_form.roomId);
+
+            room.player_2 = room_form.player
+
+            return room;
+
+        }else{
+            throw {code:403,err:"Invalid user or token or room id"}; 
         }
     }
 
